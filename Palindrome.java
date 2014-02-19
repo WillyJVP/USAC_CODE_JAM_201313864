9dcb76996d565351238cab0bb5d347e938050cc6
WillyJVP <willy-v-@hotmail.com>
2014-02-18 21:44:41 -0600

    Modifique las salidas de Palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -14,14 +14,14 @@ public class Palindrome
         String sinEspacios[]=frase.split(" "); //pasamos frase a un arreglo, quitamos los espacios
         for(i=0; i<sinEspacios.length;i++)            
             frasesinespacios+=sinEspacios[i]; //armamos una hilera que no contenga los espacios
-        System.out.println(frasesinespacios);
+       // System.out.println(frasesinespacios);
         
         sinEspacios=frasesinespacios.split("");
         
         for(i=sinEspacios.length-1; i>=0; i--)
             alReves+=sinEspacios[i]; //armamos una hilera desde la ultima posicion del arreglo ala primera
         
-        System.out.println(alReves);
+        //System.out.println(alReves);
         
         if(frasesinespacios.equals(alReves.trim())) //comparamos la frase sin espacios con la frase "al reves"
             System.out.println("Es palíndrome");
